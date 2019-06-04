81ea3ca0f55b2837317291e53e269c562e1ec6f6
唐正府 <704314516@qq.com>
2019-06-04 10:41:45 +0800

    eeeeeee

---
+++ src/main/java/com/example/demo111111111111111/Demo111111111111111Application.java
@@ -0,0 +1,16 @@
+package com.example.demo111111111111111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo111111111111111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo111111111111111Application.class, args);
+    }
+
+
+
+
+}
